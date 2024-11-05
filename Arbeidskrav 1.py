f39426985183b4c37792191f7c6e1fc616524579
tnek747 <kent@skoyter.net>
2024-11-05 11:41:50 +0100

    Update Arbeidskrav 1.py

--- Arbeidskrav 1.py
+++ Arbeidskrav 1.py
@@ -3,18 +3,24 @@
 """
 Created on Sat Oct 12 13:43:36 2024
 
-@author: KoKe
+@author: Kent
 https://github.com/tnek747/Arbeidskrav-1 
 """
+
 """Basic var"""
 km=10000
 elbil_forsikring=5000
 bensin_forsikring=7500
+trafikkforsikringsavgift=8.38
 kwh_km=0.2
 kr_kwh=2.00
 bensin_km=1.0
 elbil_bom=0.1
 bensin_bom=0.3
+
+
+"""trafikkforsikringsavgift"""
+tfa =trafikkforsikringsavgift*365
 
 """EV_var"""
 elbil_kr_km = kwh_km * kr_kwh
@@ -27,12 +33,12 @@ bensin_total_bom=bensin_bom*km
 
 """EV"""
 
-elbil_årlig_kost = elbil_forsikring + elbil_total_bom + elbil_total_km + elbil_total_km
+elbil_årlig_kost = elbil_forsikring + elbil_total_bom + elbil_total_km + elbil_total_km + tfa
 print("elbilkost pr år:")
 print(elbil_årlig_kost)
 
 """fossil"""
-bensin_årlig_kost = bensin_forsikring + bensin_total_bom + bensin_total_bensin
+bensin_årlig_kost = bensin_forsikring + bensin_total_bom + bensin_total_bensin + tfa
 print("bensinbilkost pr år")
 print(bensin_årlig_kost)
 
